fa4a55540ad0be28969be0d86f3ec45001db1086
Samanga Narayanan <74295785+samanga-n@users.noreply.github.com>
2022-11-07 18:18:20 +0530

    Update index.js

--- static/index.js
+++ static/index.js
@@ -1,10 +1,15 @@
 //Create date variable
+var date = new Date()
+let display_date= "Date:" + date.toLocaleDateString()
 
 
 //Load HTML DOM
+$(document).ready(function () {
+    $("#display_date").html(display_date)
+})
 
 //Define variable to store predicted emotion
-
+let predicted_emotion;
 
 //HTML-->JavaScript--->Flask
 //Flask--->JavaScript--->HTML
@@ -14,6 +19,10 @@
 $(function () {
     $("#predict_button").click(function () {
         //AJAX call
+         let input_data = {
+            "text": $("#text").val()
+        }
+        console.log(input_data)
 
         $.ajax({
             
@@ -21,11 +30,32 @@ $(function () {
               {
                 
                 // Result Received From Flask ----->JavaScript
+                    type: 'POST',
+                    url: "/predict-emotion",
+                    data: JSON.stringify(input_data),
+                    dataType: "json",
+                    contentType: 'application/json',
+                    success: function (result) {
                 
-                // Display Result Using JavaScript----->HTML
+                // Result Received From Flask ----->JavaScript
+                        predicted_emotion = result.data.predicted_emotion
+                        emo_url = result.data.predicted_emotion_img_url
+
+
+                        // Display Result Using JavaScript----->HTML
+                        $("#prediction").html(predicted_emotion)
+                        $('#prediction').css("display", "block");
+
+                        $("#emo_img_url").attr('src', emo_url);
+                        $('#emo_img_url').css("display", "block");
+                
+               
                 
             },
             //Error function
+                error: function (result) {
+                alert(result.responseJSON.message)
+            }
             
         });
     });
