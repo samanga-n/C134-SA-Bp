0f67ad02e9dcb562b4ef22f368ec04a6c85fc34f
Samanga Narayanan <74295785+samanga-n@users.noreply.github.com>
2022-12-08 18:57:03 +0530

    Update text_sentiment_prediction.py

--- text_sentiment_prediction.py
+++ text_sentiment_prediction.py
@@ -6,7 +6,7 @@ from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.models import load_model
 # train data
-train_data = pd.read_csv("./static/assets/data_files/tweet_emotions.csv")    
+train_data = pd.read_csv("./static/data_files/tweet_emotions.csv")    
 training_sentences = []
 
 for i in range(len(train_data)):
@@ -14,7 +14,7 @@ for i in range(len(train_data)):
     training_sentences.append(sentence)
 
 #load model
-model = load_model("./static/assets/model_files/Tweet_Emotion.h5")
+model = load_model("./static/model_files/Tweet_Emotion.h5")
 
 vocab_size = 40000
 max_length = 100
@@ -27,19 +27,19 @@ tokenizer.fit_on_texts(training_sentences)
 
 #assign emoticons for different emotions
 emo_code_url = {
-    "empty": [0, "./static/assets/emoticons/Empty.png"],
-    "sadness": [1,"./static/assets/emoticons/Sadness.png" ],
-    "enthusiasm": [2, "./static/assets/emoticons/Enthusiasm.png"],
-    "neutral": [3, "./static/assets/emoticons/Neutral.png"],
-    "worry": [4, "./static/assets/emoticons/Worry.png"],
-    "surprise": [5, "./static/assets/emoticons/Surprise.png"],
-    "love": [6, "./static/assets/emoticons/Love.png"],
-    "fun": [7, "./static/assets/emoticons/fun.png"],
-    "hate": [8, "./static/assets/emoticons/hate.png"],
-    "happiness": [9, "./static/assets/emoticons/happiness.png"],
-    "boredom": [10, "./static/assets/emoticons/boredom.png"],
-    "relief": [11, "./static/assets/emoticons/relief.png"],
-    "anger": [12, "./static/assets/emoticons/anger.png"]
+    "empty": [0, "./static/emoticons/Empty.png"],
+    "sadness": [1,"./static/emoticons/Sadness.png" ],
+    "enthusiasm": [2, "./static/emoticons/Enthusiasm.png"],
+    "neutral": [3, "./static/emoticons/Neutral.png"],
+    "worry": [4, "./static/emoticons/Worry.png"],
+    "surprise": [5, "./static/emoticons/Surprise.png"],
+    "love": [6, "./static/emoticons/Love.png"],
+    "fun": [7, "./static/emoticons/fun.png"],
+    "hate": [8, "./static/emoticons/hate.png"],
+    "happiness": [9, "./static/emoticons/happiness.png"],
+    "boredom": [10, "./static/emoticons/boredom.png"],
+    "relief": [11, "./static/emoticons/relief.png"],
+    "anger": [12, "./static/emoticons/anger.png"]
     
     }
 # write the function to predict emotion
